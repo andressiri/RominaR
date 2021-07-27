50c474f8a244a6620e94a9fbbe15bcb513f4a3fe
Andrés Siri <andres.siri@hotmail.com>
2021-07-27 17:36:42 -0300

    make lateral navbar for landscape, more responsive details and change some photos

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+const getBrowserBarHeight = () => {
+  const varClientHeight = document.querySelector(".video-header").offsetHeight;
+  const varInnerHeight = window.innerHeight;
+  return varClientHeight - varInnerHeight;
+};
+
 const burguerMenuSlide = () => {
   const burguer = document.querySelector(".burguer");
   const nav = document.querySelector(".burguer-menu");
@@ -13,11 +19,12 @@ const burguerMenuSlide = () => {
         item.style.animation = "";
       } else {
         item.style.animation = `burguerMenuFade 0.5s ease forwards ${index/25 + 0.5}s`;
-      }
+      };
     });
   });
-
-
 };
-
+//To work around browser's url bar in mobile devices.
+const browserBarHeight = getBrowserBarHeight();
+document.documentElement.style.setProperty('--browser-bar-height', `${browserBarHeight}px`);
+//Call functions.
 burguerMenuSlide();
